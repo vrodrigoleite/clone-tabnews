e3f978690bb0c1172ec3162b98be2c26d1fddef9
Victor Rodrigo Leite <vrodrigoleite@gmail.com>
2026-01-07 20:26:19 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status returns status 200 and correct body", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const reponseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(reponseBody.updated_at).toISOString();
+  expect(reponseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(reponseBody.dependencies.database.version).toEqual("16.0");
+  expect(reponseBody.dependencies.database.max_connections).toEqual(100);
+  expect(reponseBody.dependencies.database.opened_connections).toEqual(1);
+
+  console.log(reponseBody);
 });
